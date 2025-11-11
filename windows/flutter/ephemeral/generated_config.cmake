0dbc22a4a8eac75e3c29bdabfa591f061a29c2b3
Naila427 <shahadanaila@gmail.com>
2025-11-11 14:50:21 +0700

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\LENOVO\\OneDrive\\Documents\\5B_Informatika\\Mobile Programmimg\\uts\\UTS_Mobile" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "PROJECT_DIR=C:\\Users\\LENOVO\\OneDrive\\Documents\\5B_Informatika\\Mobile Programmimg\\uts\\UTS_Mobile"
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\LENOVO\\OneDrive\\Documents\\5B_Informatika\\Mobile Programmimg\\uts\\UTS_Mobile\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\LENOVO\\OneDrive\\Documents\\5B_Informatika\\Mobile Programmimg\\uts\\UTS_Mobile"
+  "FLUTTER_TARGET=C:\\Users\\LENOVO\\OneDrive\\Documents\\5B_Informatika\\Mobile Programmimg\\uts\\UTS_Mobile\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuMzUuMw==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049YTQwMmQ5YTQzNw==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049ZGRmNDdkZDNmZg==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My45LjI="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\LENOVO\\OneDrive\\Documents\\5B_Informatika\\Mobile Programmimg\\uts\\UTS_Mobile\\.dart_tool\\package_config.json"
+)
